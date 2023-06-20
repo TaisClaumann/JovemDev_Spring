2c86709aa6e4fad28281c98663fa259b454f07d7
Taís Marcolino <tais.c.marcolino@gmail.com>
2023-06-20 14:00:19 -0300

    Implementação de testes resource para User

--- src/test/java/br/com/trier/spring_matutino/resources/UserResourceTest.java
+++ src/test/java/br/com/trier/spring_matutino/resources/UserResourceTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,7 @@ import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import br.com.trier.spring_matutino.SpringMatutinoApplication;
 import br.com.trier.spring_matutino.domain.User;
 import br.com.trier.spring_matutino.domain.dto.UserDTO;
+import br.com.trier.spring_matutino.services.impl.UserServiceImpl;
 
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = Replace.ANY)
@@ -35,6 +37,9 @@ public class UserResourceTest {
 	
 	@Autowired
 	protected TestRestTemplate rest;
+	
+	@Autowired
+	UserServiceImpl service;
 
 	private ResponseEntity<UserDTO> getUser(String url) { //converte o json
 		return rest.getForEntity(url, UserDTO.class);
@@ -105,9 +110,33 @@ public class UserResourceTest {
 	
 	@Test
 	@DisplayName("Teste delete")
+	@Sql({"classpath:/resources/sqls/limpa_tabelas.sql"})
+	@Sql({"classpath:/resources/sqls/usuario.sql"})
 	public void deleteTest() {
-		int id = 1;
-		ResponseEntity<Void> response = rest.exchange("/user/{id}", HttpMethod.DELETE, null, Void.class, id);
+		ResponseEntity<Void> response = rest.exchange("/user/1", HttpMethod.DELETE, null, Void.class);
+		assertEquals(response.getStatusCode(), HttpStatus.OK);
+	}
+	
+	@Test
+	@DisplayName("Teste achar usuario pelo nome")
+	public void findByNameTest() {
+		ResponseEntity<UserDTO> response = getUser("/user/name/Usuario Test 1");
+		assertEquals(response.getStatusCode(), HttpStatus.OK);
+	}
+	
+	@Test
+	@DisplayName("Teste achar usuario pelo email")
+	public void findByEmailTest() {
+		ResponseEntity<UserDTO> response = getUser("/user/email/test2@teste.com.br");
+		assertEquals(response.getStatusCode(), HttpStatus.OK);
+	}
+	
+	@Test
+	@DisplayName("Teste achar usuarios pelo nome com like ignorando o case")
+	@Sql({"classpath:/resources/sqls/limpa_tabelas.sql"})
+	@Sql({"classpath:/resources/sqls/usuario.sql"})
+	public void findByNameContainsIgnoreCaseTest() {
+		ResponseEntity<List<UserDTO>> response = getUsers("/user/like/usuario");
 		assertEquals(response.getStatusCode(), HttpStatus.OK);
 	}
 }
