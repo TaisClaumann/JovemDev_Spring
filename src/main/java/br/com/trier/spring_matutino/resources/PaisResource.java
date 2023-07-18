865b45f3e109a302e940a9ad7698e7bfa521c279
Taís Marcolino <tais.c.marcolino@gmail.com>
2023-07-18 11:58:48 -0300

    Implementação do headers

--- src/main/java/br/com/trier/spring_matutino/resources/PaisResource.java
+++ src/main/java/br/com/trier/spring_matutino/resources/PaisResource.java
@@ -24,45 +24,45 @@ public class PaisResource {
 	@Autowired
 	private PaisService service;
 	
-	@Secured({"ROLE_ADMIN"})
+	//@Secured({"ROLE_ADMIN"})
 	@PostMapping
 	public ResponseEntity<Pais> insert(@RequestBody Pais pais){
 		return ResponseEntity.ok(service.insert(pais));
 	}
 	
-	@Secured({"ROLE_ADMIN"})
+	//@Secured({"ROLE_ADMIN"})
 	@PutMapping("/{id}")
 	public ResponseEntity<Pais> update(@PathVariable Integer id, @RequestBody Pais pais){
 		pais.setId(id);
 		return ResponseEntity.ok(service.update(pais));
 	}
 	
-	@Secured({"ROLE_ADMIN"})
+	//@Secured({"ROLE_ADMIN"})
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Integer id){
 		service.delete(id);
 		return ResponseEntity.ok().build();
 	}
 	
-	@Secured({"ROLE_USER"})
+	//@Secured({"ROLE_USER"})
 	@GetMapping()
 	public ResponseEntity<List<Pais>> listAll(){
 		return ResponseEntity.ok(service.listAll());
 	}
 	
-	@Secured({"ROLE_USER"})
+	//@Secured({"ROLE_USER"})
 	@GetMapping("/{id}")
 	public ResponseEntity<Pais> findById(@PathVariable Integer id){
 		return ResponseEntity.ok(service.findById(id));
 	}
 
-	@Secured({"ROLE_USER"})
+	//@Secured({"ROLE_USER"})
 	@GetMapping("/name/{name}")
 	public ResponseEntity<Pais> findByNameEqualsIgnoreCase(@PathVariable String name){
 		return ResponseEntity.ok(service.findByNameEqualsIgnoreCase(name));
 	}
 	
-	@Secured({"ROLE_USER"})
+	//@Secured({"ROLE_USER"})
 	@GetMapping("/like/{name}")
 	public ResponseEntity<List<Pais>> findByNameContainsIgnoreCase(@PathVariable String name){
 		return ResponseEntity.ok(service.findByNameContainsIgnoreCase(name));
